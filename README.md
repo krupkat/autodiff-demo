6736fc6e8a3dbd75c34eab90706b201849a61277
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2026-05-22 23:50:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AutoDiff demo
+
+## Build
+
+```
+cmake -B build . -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmake --build build
+```
---
+++ default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> { };
+
+mkShell {
+  packages = [
+    # tools
+    cmake
+    clang-tools
+    emscripten
+    ninja
+    pkgconf
+    # libraries
+    libGL.dev
+    libx11.dev
+    libxi.dev
+    libxcursor.dev
+    libxcb.dev
+    libxdmcp.dev
+    libxrender.dev
+  ];
+}
